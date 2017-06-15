1c5d0552f8fa0f6cf6173f3b80753a6f60e7c063
magallanesdn <magallanesdn@gmail.com>
2017-06-15 15:49:42 +0000

    modify user control and user edit view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,22 +7,19 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  # GET /users/1
-  # GET /users/1.json
   def show
   end
 
-  # GET /users/new
+ 
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
+
   def edit
+    @user = User.find(params[:id])
   end
 
-  # POST /users
-  # POST /users.json
   def create
     @user = User.new(user_params)
     
